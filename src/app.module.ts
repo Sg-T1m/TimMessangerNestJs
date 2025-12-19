5c1aff75da92720f3aa52fb9a9f457dfb871e1e3
Sg-T1m <perevozchikov.t.a@gmail.com>
2025-12-19 22:59:35 +0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'messenger_db',
+
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+
+      synchronize: true, // ⚠️ только для разработки
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
